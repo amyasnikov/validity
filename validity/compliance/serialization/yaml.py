2c52666da0f6eb0f28b3bb47fc969ed3dc26830d
Anton M <anton2008m@gmail.com>
2024-02-23 00:31:07 +0500

    bugfixes

--- validity/compliance/serialization/yaml.py
+++ validity/compliance/serialization/yaml.py
@@ -1,6 +1,5 @@
 import yaml
 
-from validity.utils.json import transform_json
 from validity.utils.misc import reraise
 from ..exceptions import SerializationError
 from .common import postprocess_jq
@@ -9,11 +8,4 @@ from .common import postprocess_jq
 @postprocess_jq
 def serialize_yaml(plain_data: str, template: str, parameters: dict) -> dict:
     with reraise(yaml.YAMLError, SerializationError, "Got invalid JSON/YAML"):
-        result = yaml.safe_load(plain_data)
-    if parameters.get("drop_attributes"):
-        result = transform_json(
-            result,
-            match_fn=lambda key, _: isinstance(key, str) and key.startswith("@"),
-            transform_fn=lambda key, value: None,
-        )
-    return result
+        return yaml.safe_load(plain_data)
--- validity/utils/json.py
+++ validity/utils/json.py
@@ -19,20 +19,26 @@ def transform_json(data: Json, match_fn: Callable[[int | str, Json], bool], tran
         if isinstance(data_item, str) or not isinstance(data_item, Collection):
             return
         iterator = data_item.items() if isinstance(data_item, dict) else enumerate(data_item)
+        delete_keys = []
+        new_values = {}
         for key, value in iterator:
             if match_fn(key, value):
                 result = transform_fn(key, value)
                 if result is None:
-                    del data_item[key]
+                    delete_keys.append(key)
                     continue
                 new_key, new_value = result
                 if new_key == key:
                     data_item[key] = new_value
                 else:
-                    del data_item[key]
-                    data_item[new_key] = new_value
+                    delete_keys.append(key)
+                    new_values[new_key] = new_value
             elif isinstance(value, Collection):
                 transform(value)
+        for del_key in delete_keys:
+            del data_item[del_key]
+        for new_key, new_value in new_values.items():
+            data_item[new_key] = new_value
 
     data_copy = copy.deepcopy(data)
     transform(data_copy)
