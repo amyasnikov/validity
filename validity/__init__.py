4e690717f0645b774caa94be7caf68fde98bfe7e
Anton M <anton2008m@gmail.com>
2024-10-18 22:12:56 +0200

    bump version

--- validity/__init__.py
+++ validity/__init__.py
@@ -21,7 +21,7 @@ class NetBoxValidityConfig(PluginConfig):
     description = "Write and run auto tests for network devices"
     author = "Anton Miasnikov"
     author_email = "anton2008m@gmail.com"
-    version = "3.0.4"
+    version = "3.0.5"
     base_url = "validity"
     django_apps = ["django_bootstrap5"]
     min_version = "3.7.0"
