beb16c47127a53ba9d77659eb283c8e3a86fe2cb
Anton M <anton2008m@gmail.com>
2026-03-20 03:04:35 +0500

    try another version combination

--- validity/netbox_changes/__init__.py
+++ validity/netbox_changes/__init__.py
@@ -10,7 +10,7 @@ from strawberry_django import FilterLookup
 from validity import config
 
 
-StrFilterLookup = locate("strawberry_django.StrFilterLookup") if config.netbox_version >= "4.5.4" else FilterLookup[str]
+StrFilterLookup = locate("strawberry_django.StrFilterLookup") if config.netbox_version >= "4.5.5" else FilterLookup[str]
 
 if config.netbox_version >= "4.5.0":
     from .current import *
